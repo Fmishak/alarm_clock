4f61f4146f459b35a7ba28473acd1ddafa4092fc
Farouk Mishak <farouk.mishak@gmail.com>
2018-11-12 17:03:05 -0800

    using namespace std;

--- alarm-clock/main.cpp
+++ alarm-clock/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World  testing!\n";
+    cout << " Thank you!" << endl;
     return 0;
 }
