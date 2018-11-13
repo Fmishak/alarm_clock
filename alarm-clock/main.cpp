1829d2e8cf9f0f13f3f60807433a86da53fe2ad2
Farouk Mishak <farouk.mishak@gmail.com>
2018-11-12 16:44:24 -0800

    Initial Commit

---
+++ alarm-clock/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  alarm-clock
+//
+//  Created by Farouk Mishak on 11/12/18.
+//  Copyright © 2018 Farouk Mishak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
